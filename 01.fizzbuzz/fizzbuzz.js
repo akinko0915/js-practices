90d562f697bd1fd3e1c70f7b91730b285a8f8730
Aki Nakahara <akinakahara1515@gmail.com>
2025-06-05 09:08:38 +0900

    :+1: 厳密等価演算子に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,16 +2,16 @@
 
 let count = 0
 while (count < 20) {
-    count++
-    if(count % 15 == 0)
-        console.log("FizzBuzz");
-    else if (count % 3 == 0) {
-        console.log("Fizz");
-    } 
-    else if (count % 5 == 0){
-        console.log("Buzz");
-    }
-    else {
-        console.log(String(count));
-    }
+  count++
+  if(count % 15 === 0)
+		console.log("FizzBuzz");
+  else if (count % 3 === 0) {
+    console.log("Fizz");
+  }
+  else if (count % 5 === 0){
+    console.log("Buzz");
+  }
+  else {
+    console.log(String(count));
+  }
 }
