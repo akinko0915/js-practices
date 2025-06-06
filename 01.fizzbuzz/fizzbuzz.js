410f8422e324a88fd7bb5c6afc6a1f5a58ece97a
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-06 12:15:16 +0900

    :shirt: Format codes with prettier playground

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,15 +3,12 @@
 let count = 0
 while (count < 20) {
   count++
-  if(count % 15 === 0)
-		console.log("FizzBuzz");
+  if (count % 15 === 0) console.log("FizzBuzz")
   else if (count % 3 === 0) {
-    console.log("Fizz");
-  }
-  else if (count % 5 === 0){
-    console.log("Buzz");
-  }
-  else {
-    console.log(String(count));
+    console.log("Fizz")
+  } else if (count % 5 === 0) {
+    console.log("Buzz")
+  } else {
+    console.log(String(count))
   }
 }
